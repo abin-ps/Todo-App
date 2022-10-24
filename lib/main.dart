0460239805b7d69cff8fc191e0cca32b8c2efe6f
Abin P S <abinps23@gmail.com>
2022-10-24 13:56:32 +0530

    Initial Commit - starting point of todo app

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TodoApp());
+}
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Todo App"),
+        ),
+      ),
+    );
+  }
+}
