f59b0b702579a0c059b27f2bdf9adf572e99c31e
Abin P S <abinps23@gmail.com>
2022-10-24 15:47:18 +0530

    sample task list created

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,19 @@ void main() {
   runApp(TodoApp());
 }
 
-class TodoApp extends StatelessWidget {
+class TodoApp extends StatefulWidget {
   const TodoApp({super.key});
 
+  @override
+  State<TodoApp> createState() => _TodoAppState();
+}
+
+class _TodoAppState extends State<TodoApp> {
+  List _taskList = [
+    "complete js project within one week",
+    "start learning node js as soon as possible"
+  ];
+  bool _doneTask = false;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,7 +24,30 @@ class TodoApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Todo App"),
         ),
+        body: _buildTodoList(),
       ),
+    );
+  }
+
+  Widget _buildTodoList() {
+    return ListView.builder(
+      itemCount: _taskList.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(
+            "${_taskList[index]}",
+            style: _doneTask
+                ? TextStyle(
+                    color: Colors.grey, decoration: TextDecoration.lineThrough)
+                : TextStyle(color: Colors.black),
+          ),
+          onTap: () {
+            setState(() {
+              _doneTask = !_doneTask;
+            });
+          },
+        );
+      },
     );
   }
 }
