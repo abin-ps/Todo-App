4add55b44d0b6173093131dd63c0e7e24c083543
Abin P S <abinps23@gmail.com>
2022-10-24 22:16:03 +0530

    code optimization and bug fixes

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -31,7 +31,10 @@ class _HomeState extends State<Home> {
       body: _buildTodoList(),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add_task_outlined),
-        onPressed: _buildShowDialog,
+        onPressed: (){
+          _TaskController.clear();
+          _buildShowDialog();
+        },
       ),
     );
   }
@@ -40,57 +43,65 @@ class _HomeState extends State<Home> {
     return ListView.builder(
       itemCount: _taskList.length,
       itemBuilder: (context, index) {
-        return ListTile(
-          leading: Icon(_taskList[index]["doneTaskFlag"]
-              ? Icons.check_box_outlined
-              : Icons.check_box_outline_blank),
-          title: Text(
-            "${_taskList[index]["title"]}",
-            style: _taskList[index]["doneTaskFlag"]
-                ? TextStyle(
-                    color: Colors.grey, decoration: TextDecoration.lineThrough)
-                : TextStyle(color: Colors.black),
+        return Padding(
+          padding: const EdgeInsets.fromLTRB(10, 8, 10, 0),
+          child: ListTile(
+            leading: Icon(_taskList[index]["doneTaskFlag"]
+                ? Icons.check_box_outlined
+                : Icons.check_box_outline_blank),
+            title: Text(
+              "${_taskList[index]["title"]}",
+              style: _taskList[index]["doneTaskFlag"]
+                  ? TextStyle(
+                      color: Colors.grey,
+                      decoration: TextDecoration.lineThrough)
+                  : TextStyle(color: Colors.black),
+            ),
+            onTap: () {
+              setState(() {
+                _taskList[index]["doneTaskFlag"] =
+                    !_taskList[index]["doneTaskFlag"];
+              });
+            },
           ),
-          onTap: () {
-            setState(() {
-              _taskList[index]["doneTaskFlag"] =
-                  !_taskList[index]["doneTaskFlag"];
-            });
-          },
         );
       },
     );
   }
 
-  Future _buildShowDialog(){
-   return showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                title: Column(
-                  children: [
-                    TextField(
-                      controller: _TaskController,
-                    ),
-                    SizedBox(height: 10),
-                    TextButton(
-                        onPressed: () {
-                          (_TaskController.text != "")
-                              ? setState(() {
-                                  _taskList.add({
-                                    "title": _TaskController.text,
-                                    "doneTaskFlag": false
-                                  });
-                                  _TaskController.clear();
-                                  Navigator.pop(context);
-                                })
-                              : Navigator.pop(context);
-                        },
-                        child: Text("Save"))
-                  ],
-                ),
-              );
-            },
-          );
+  Future _buildShowDialog() {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Column(
+            children: [
+              TextField(
+                controller: _TaskController,
+                // textAlign: TextAlign.center,
+                autofocus: true,
+                maxLength: 25,
+                onSubmitted: (value){
+                  _saveNewTask();
+                },
+              ),
+              SizedBox(height: 15),
+              TextButton(onPressed: _saveNewTask, child: Text("Save"))
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  _saveNewTask() {
+    (_TaskController.text != "")
+        ? setState(() {
+            _taskList
+                .add({"title": _TaskController.text, "doneTaskFlag": false});
+            _TaskController.clear();
+            Navigator.pop(context);
+          })
+        : Navigator.pop(context);
   }
 }
